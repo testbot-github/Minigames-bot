091ec05d572610f9d12695bc9efb69abee20e62f
Mathiscool <82745607+testbot-github@users.noreply.github.com>
2021-11-25 14:10:51 +0530

    Update help.js

--- commands/Information/help.js
+++ commands/Information/help.js
@@ -24,7 +24,7 @@ module.exports = {
           const embed = new MessageEmbed();
           const cmd = client.commands.get(args[0].toLowerCase()) || client.commands.get(client.aliases.get(args[0].toLowerCase()));
           if (!cmd) {
-              return message.reply({embeds: [embed.setColor(ee.wrongcolor).setDescription(`No Information found for command **${args[0].toLowerCase()}**`)]});
+              return message.channel.send(embed.setColor(ee.wrongcolor).setDescription(`No Information found for command **${args[0].toLowerCase()}**`));
           }
           if (cmd.name) embed.addField("**Command name**", `\`${cmd.name}\``);
           if (cmd.name) embed.setTitle(`Detailed Information about:\`${cmd.name}\``);
@@ -36,7 +36,7 @@ module.exports = {
               embed.addField("**Usage**", `\`${prefix}${cmd.usage}\``);
               embed.setFooter("Syntax: <> = required, [] = optional");
           }
-          return message.reply({embeds: [embed.setColor(ee.color)]});
+          return message.channel.send({embed: embed.setColor(ee.color)});
         } else {
           const embed = new MessageEmbed()
               .setColor(ee.color)
@@ -55,16 +55,16 @@ module.exports = {
           } catch (e) {
               console.log(String(e.stack).red);
           }
-          message.reply({embeds: [embed]});
+          message.channel.send({embed: embed});
       }
     } catch (e) {
         console.log(String(e.stack).bgRed)
-        return message.reply({embeds: [new MessageEmbed()
+        return message.channel.send(new MessageEmbed()
             .setColor(ee.wrongcolor)
             .setFooter(ee.footertext, ee.footericon)
             .setTitle(`❌ ERROR | An error occurred`)
-            .setDescription(`\`\`\`${e.message ? String(e.message).substr(0, 2000) : String(e).substr(0, 2000)}\`\`\``)
-        ]});
+            .setDescription(`\`\`\`${e.stack}\`\`\``)
+        );
     }
   }
 }
